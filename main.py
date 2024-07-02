2f853dc3f6a311c6c56bb7a7d1bae0c2e8201d74
Dev <shunya.ekaya01@gmail.com>
2024-07-02 13:01:04 +0530

    UPDATE: increasing rate limits;

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ with app.app_context():
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["500 per day", "50 per hour"]
+    default_limits=["1000 per day", "100 per hour"]
 )
 
 SWAGGER_URL = '/api/docs'
